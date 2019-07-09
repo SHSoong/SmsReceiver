708b9dcf6a9be990cb2ef347c9dd71b6be704178
soong <iamssh@126.com>
2019-07-09 15:56:52 +0800

    覆盖文件

--- app/src/main/java/com/ssh/smsreceiver/MainActivity.kt
+++ app/src/main/java/com/ssh/smsreceiver/MainActivity.kt
@@ -5,12 +5,17 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
 import android.os.Message
 import android.support.v7.app.AppCompatActivity
 import android.telephony.SmsMessage
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 import java.io.IOException
+import java.nio.file.Files.delete
+import java.nio.file.Files.exists
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,10 +30,11 @@ class MainActivity : AppCompatActivity() {
 
         filter = IntentFilter()
         filter?.addAction("android.provider.Telephony.SMS_RECEIVED")
-        receiver = SmsReceiver(updateUI)
-        registerReceiver(receiver, filter)
 
         tv_content.text = readFile()
+
+        receiver = SmsReceiver(updateUI)
+        registerReceiver(receiver, filter)
     }
 
     private fun writeFile(txt: String) {
@@ -58,14 +64,27 @@ class MainActivity : AppCompatActivity() {
         return txt
     }
 
+    /**
+     * 删除单个文件
+     * @param   filePath    被删除文件的文件名
+     * @return 文件删除成功返回true，否则返回false
+     */
+    override fun deleteFile(filePath: String): Boolean {
+        val file = File(filePath)
+        return if (file.isFile && file.exists()) {
+            file.delete()
+        } else false
+    }
+
     private var updateUI: Handler = object : Handler() {
 
         override fun handleMessage(msg: Message) {
             super.handleMessage(msg)
             if (msg.what == 1) {
                 val txt = msg.data.getString("msg")
-                tv_content.text = txt
-                writeFile(txt)
+                if (deleteFile(filesDir.absolutePath + "/" + fileName))
+                    writeFile(txt)
+                tv_content.text = readFile()
             }
         }
     }
@@ -82,12 +101,10 @@ class MainActivity : AppCompatActivity() {
                     content.append(message.messageBody)//获取短信内容
                 }
             }
-            var text = ""
-            text = "$text 新短信：$content\n"
             val msg = Message()
             msg.what = 1
             val b = Bundle()
-            b.putString("msg", text)
+            b.putString("msg", "新短信：$content\n\n")
             msg.data = b
             handler.sendMessage(msg)
         }
